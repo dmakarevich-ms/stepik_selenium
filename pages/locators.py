ba4b2a452179f30f20750dd048f76e550fd90549
d.makarevich <d.makarevich@moonactive.com>
2025-05-15 15:45:41 +0300

    adding locators for login page, update tests

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+
+    LOGIN_FORM = (By.ID, 'login_form')
+    LOGIN_USER_MAIL = (By. NAME, 'login-username')
+    PASS_TO_LOGIN = (By.NAME, "login-password")
+    LOGIN_BUTTON = (By.NAME, 'login_submit')
+    REGISTRATION_FORM = (By.ID, 'register_form')
+    REGISTER_USER_MAIL = (By.NAME, 'registration-email')
+    PASS_TO_REGISTR = (By.ID, 'id_registration-password1')
+    REPEAT_PASS_TO_REGISTER =(By.ID, 'id_registration-password2')
+    REGISTRATION_BUTTON = (By.NAME, 'registration_submit')
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,7 @@
 import time
-
 from selenium.common import NoSuchElementException
-
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -15,3 +14,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_url_presented(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
